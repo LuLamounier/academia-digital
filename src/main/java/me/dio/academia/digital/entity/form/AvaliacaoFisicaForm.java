e6d47f1d4807e11f62351b34683b3cb94134c85f
Luanna Lopes Dias Lamounier <ldluanna@gmail.com>
2023-04-16 19:54:11 -0300

    Passo a passo projeto Spring Data JPA

--- src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
@@ -8,16 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AvaliacaoFisicaForm {
-
-  //@Positive(message = "O Id do aluno precisa ser positivo.")
+	
   private Long alunoId;
-
-  //@NotNull(message = "Preencha o campo corretamente.")
-  //@Positive(message = "${validatedValue}' precisa ser positivo.")
   private double peso;
-
-  //@NotNull(message = "Preencha o campo corretamente.")
-  //@Positive(message = "${validatedValue}' precisa ser positivo.")
-  //@DecimalMin(value = "150", message = "'${validatedValue}' precisa ser no mínimo {value}.")
   private double altura;
 }
